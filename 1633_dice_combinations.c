ea083b61b7c4a7fdc2936e614cb7c50909b43a6d
Mustafa Furkan Bulut <mfbulut2006@gmail.com>
2025-11-25 14:48:07 +0300

    dice combinations

---
+++ 1633_dice_combinations.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+#pragma GCC optimize("O3")
+typedef char* string;
+typedef int i32;
+typedef unsigned int u32;
+typedef long long i64;
+typedef unsigned long long u64;
+#define range(i,y,x) for(i64 i=(y);(i<=(x));(i)++)
+#define min(a, b) ((a) > (b) ? (b) : (a))
+
+// since we know n <= 10^6
+i64 memo[1000001];
+
+i64 combinations(i64 n) {
+    if(memo[n] != -1) {
+        return memo[n];
+    }
+
+    i64 result = 0;
+
+    if(n <= 6) { result = 1; }
+
+    range(i, 1, min(n - 1, 6)) {
+        result = (result + combinations(n - i)) % 1000000007;
+    }
+
+    memo[n] = result;
+    return result;
+}
+
+int main() {
+    for(int i = 0; i < 1000001; ++i) {
+        memo[i] = -1;
+    }
+
+    memo[1] = 1;
+
+    i64 n;
+    scanf("%lld", &n);
+
+    // printf("1: %lld \n", combinations(1));
+    // printf("2: %lld \n", combinations(2));
+    // printf("3: %lld \n", combinations(3));
+    // printf("4: %lld \n", combinations(4));
+    // printf("5: %lld \n", combinations(5));
+    // printf("6: %lld \n", combinations(6));
+    // printf("7: %lld \n", combinations(7));   // Answer: 63
+    // printf("8: %lld \n", combinations(8));   // Answer: 125
+    // printf("9: %lld \n", combinations(9));   // Answer: 248
+    // printf("10: %lld \n", combinations(10)); // Answer: 492
+
+    i64 result = combinations(n);
+    printf("%lld", result);
+}
--- 1754_coin_piles.c
+++ 1754_coin_piles.c
@@ -5,10 +5,7 @@ typedef int i32;
 typedef unsigned int u32;
 typedef long long i64;
 typedef unsigned long long u64;
-#define range(i,y,x) for(i64 i=(y);(((x)>=(y))?(i<=(x)):(i>=x));(((x)>=(y))?((i)++):((i)--)))
-#define foreach(i, A) for(int _keep=1, _count=0,_size=sizeof (A) / sizeof *(A); _keep && _count != _size; _keep = !_keep, _count++) for(i = (A)+_count; _keep; _keep = !_keep)
-#define len(a) (sizeof(a) / sizeof((a)[0]))
-#define log(x, fmt, ...) if(x){printf("%s@%d: " fmt "\n", __FILE__, __LINE__,__VA_ARGS__);}
+#define range(i,y,x) for(i64 i=(y);(i<=(x));(i)++)
 
 int main() {
     i64 n;
